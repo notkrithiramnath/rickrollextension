7ebcc059b23f2c2b5fcb7a2d23627edfc6966848
Krithi Ramnath <114512556+notkrithiramnath@users.noreply.github.com>
2025-06-03 23:05:50 -0400

    Add files via upload

---
+++ Content.js
@@ -0,0 +1,18 @@
+let imageURL = "https://tse4.mm.bing.net/th?id=OIP.cxdH6hvbuXK1tZqIp7e6GgHaGA&pid=Api";
+
+function replaceImages() {
+  const imgs = document.getElementsByTagName("img");
+  for (let img of imgs) {
+    img.src = imageURL;
+  }
+}
+
+// Run once on load
+replaceImages();
+
+// Re-run anytime the DOM changes
+const observer = new MutationObserver(replaceImages);
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
